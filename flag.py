d658bd0bb018f5fe2a0e6ecd1bc415d2a2231064
BllsVGB <slavioglonikita@gmail.com>
2024-10-27 18:38:17 +0300

    удалены лишние комментарии

--- flag.py
+++ flag.py
@@ -19,24 +19,3 @@ def draw_flag_line():
 
 if __name__ == "__main__":
     draw_flag_line()
-
-
-# # узор
-# print('№2 Узор е')
-
-
-# def draw_pattern(color1 = 232, color2 = 231):
-#     print(f"{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*11}{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*1}{END}")
-#     print(f"{SET_COLOR}{color1}m{' '*2}{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*7}{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*3}{END}")
-#     print(f"{SET_COLOR}{color1}m{' '*4}{SET_COLOR}{color2}m{' '*2}{SET_COLOR}{color1}m{' '*5}{SET_COLOR}{color2}m{' '*2}{SET_COLOR}{color1}m{' '*5}{END}")
-#     print(f"{SET_COLOR}{color1}m{' '*6}{SET_COLOR}{color2}m{' '*2}{SET_COLOR}{color1}m{' '*1}{SET_COLOR}{color2}m{' '*2}{SET_COLOR}{color1}m{' '*7}{END}")
-#     print(f"{SET_COLOR}{color1}m{' '*7}{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*8}{END}")
-#     print(f"{SET_COLOR}{color1}m{' '*6}{SET_COLOR}{color2}m{' '*2}{SET_COLOR}{color1}m{' '*1}{SET_COLOR}{color2}m{' '*2}{SET_COLOR}{color1}m{' '*7}{END}")
-#     print(f"{SET_COLOR}{color1}m{' '*4}{SET_COLOR}{color2}m{' '*2}{SET_COLOR}{color1}m{' '*5}{SET_COLOR}{color2}m{' '*2}{SET_COLOR}{color1}m{' '*5}{END}")
-#     print(f"{SET_COLOR}{color1}m{' '*2}{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*7}{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*3}{END}")
-#     print(f"{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*11}{SET_COLOR}{color2}m{' '*3}{SET_COLOR}{color1}m{' '*1}{END}")
-
-
-# if __name__ == "__main__":
-#     for i in range(6):
-#         draw_pattern()
